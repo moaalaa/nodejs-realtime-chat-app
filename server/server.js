d3097d7da9e213c879fee151a20d4af75950c94b
moaalaa <moaalaa16@gmail.com>
2019-05-29 15:24:39 +0200

    Creating Socket Connect And Disconnect Connection handler

--- server/server.js
+++ server/server.js
@@ -1,24 +1,39 @@
-// Paths
-
-const path = require('path');
-const publicPath = path.join(__dirname, '..', '/public');
-
 // Modules                                  
+const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io')
 const express = require('express');
 const bodyParser = require('body-parser');
 require('express-group-routes');
 
+// Public Path
+const publicPath = path.join(__dirname, '..', '/public');
+
 // Init Express App                         
 const app = express();
+
+// Init Http Server For Socket.io
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// Set App Static Directory
+app.use(express.static(publicPath))
+
+// Use Body Parser                          
+app.use(bodyParser.json());
 
 // Port                                     
 let port = process.env.PORT || 3000;
 
-// Set App Static Directory
-app.use(express.static(publicPath))
-// Use Body Parser                          
-app.use(bodyParser.json());
-
 // Start Http Server
-app.listen(port, () => console.log(`Server Started on Port ${port}`));
+server.listen(port, () => console.log(`Server Started on Port ${port}`));
 
+
+// Socket.io Connections
+io.on('connection', socket => {
+    console.log('New User Connected', socket.client.id);
+
+    socket.on('disconnect', () => {
+        console.log("Client Disconnected", socket.client.id);
+    });
+})
